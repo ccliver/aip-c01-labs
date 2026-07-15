2c40c8ab9bb7b25449ec006f5cc50d28a41a048b
Carl Cliver <ccliver@gmail.com>
2026-07-14 21:08:36 -0400

    chore: cleanup

--- modules/iam_roles/main.tf
+++
@@ -1,7 +0,0 @@
-# Shared IAM roles module — reusable execution roles for Lambda and other compute
-# resources that need access to Amazon Bedrock.
-
-# TODO: aws_iam_role (lambda_bedrock_exec) with trust policy for lambda.amazonaws.com
-# TODO: aws_iam_role_policy_attachment — AWSLambdaBasicExecutionRole managed policy
-# TODO: aws_iam_policy — bedrock:InvokeModel, bedrock:InvokeModelWithResponseStream
-# TODO: aws_iam_role_policy_attachment — attach Bedrock policy to Lambda role
--- modules/s3/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "bucket_id" {
-  description = "Name of the created S3 bucket"
-  value       = null # TODO: replace with aws_s3_bucket.this.id
-}
-
-output "bucket_arn" {
-  description = "ARN of the created S3 bucket"
-  value       = null # TODO: replace with aws_s3_bucket.this.arn
-}
